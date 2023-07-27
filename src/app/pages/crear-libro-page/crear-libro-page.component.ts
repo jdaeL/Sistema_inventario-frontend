2d816bea58a78cb8112a5f2b109df72f04b1550b
Omar <oromero@unsa.edu.pe>
2023-07-26 20:18:55 -0500

    Se agregó el formulario de crear libros

--- src/app/pages/crear-libro-page/crear-libro-page.component.ts
+++ src/app/pages/crear-libro-page/crear-libro-page.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./crear-libro-page.component.css']
 })
 export class CrearLibroPageComponent {
-
+    addLibro(titulo: any,
+      ano_publicacion: any,
+      precio: any,
+      imagen: any,
+      descripcion: any,
+      stock: any,
+      firmado: any,
+      hay_envio_gratis: any,
+      autor: any):boolean{
+    return false;
+  }
 }
