7ba9f3b33f18fab1d54c2cc895d8f21c556a0b15
CieloMV <cmezav@unsa.edu.pe>
2023-08-04 22:30:38 -0500

    Se realizó la captura del id del libro para editar

--- src/app/pages/editar-libro-page/editar-libro-page.component.ts
+++ src/app/pages/editar-libro-page/editar-libro-page.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LibrosService } from "../../libros.service";
+import { Autor } from "../../autor";
 
 @Component({
   selector: 'app-editar-libro-page',
@@ -6,5 +9,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./editar-libro-page.component.css']
 })
 export class EditarLibroPageComponent {
-
+  constructor(private route: ActivatedRoute, private librosService: LibrosService, private router: Router){
+    this.route.params?.subscribe((params: any) => {
+      const id=params.id
+    })
+  }  
 }
